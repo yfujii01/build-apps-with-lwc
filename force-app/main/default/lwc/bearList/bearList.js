dbe94f337ca3c6934076ef01666d8c506a53ad6f
yfujii01 <yfujii01@gmail.com>
2020-01-22 22:42:52 +0900

    Lesson3 途中

---
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -0,0 +1,23 @@
+import { LightningElement, track } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.getAllBears() Apex method */
+import getAllBears from '@salesforce/apex/BearController.getAllBears';
+export default class BearList extends LightningElement {
+	@track bears;
+	@track error;
+	appResources = {
+		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
+	};
+	connectedCallback() {
+		this.loadBears();
+	}
+	loadBears() {
+		getAllBears()
+			.then(result => {
+				this.bears = result;
+			})
+			.catch(error => {
+				this.error = error;
+			});
+	}
+}
