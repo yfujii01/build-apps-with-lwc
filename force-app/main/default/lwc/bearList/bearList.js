a5084afaab390fd570ba43fa6a9855293161202b
yfujii01 <yfujii01@gmail.com>
2020-01-23 21:51:03 +0900

    add Lesson4

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,25 +1,27 @@
-import { LightningElement, track, wire } from 'lwc';
-import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import { loadStyle } from "lightning/platformResourceLoader";
+import { LightningElement, track, wire } from "lwc";
+import ursusResources from "@salesforce/resourceUrl/ursus_park";
 /** BearController.searchBears(searchTerm) Apex method */
-import searchBears from '@salesforce/apex/BearController.searchBears';
+import searchBears from "@salesforce/apex/BearController.searchBears";
 export default class BearList extends LightningElement {
-	@track searchTerm = '';
-	@wire(searchBears, {searchTerm: '$searchTerm'})
-	bears;
-	appResources = {
-		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
-	};
-	handleSearchTermChange(event) {
-		// Debouncing this method: do not update the reactive property as
-		// long as this function is being called within a delay of 300 ms.
-		// This is to avoid a very large number of Apex method calls.
-		window.clearTimeout(this.delayTimeout);
-		const searchTerm = event.target.value;
-		this.delayTimeout = setTimeout(() => {
-			this.searchTerm = searchTerm;
-		}, 300);
-	}
-	get hasResults() {
-		return (this.bears.data.length > 0);
-	}
+  @track searchTerm = "";
+  @wire(searchBears, { searchTerm: "$searchTerm" })
+  bears;
+  connectedCallback() {
+    loadStyle(this, ursusResources + "/style.css");
+  }
+  handleSearchTermChange(event) {
+    // Debouncing this method: do not update the reactive property as
+    // long as this function is being called within a delay of 300 ms.
+    // This is to avoid a very large number of Apex method calls.
+    window.clearTimeout(this.delayTimeout);
+    const searchTerm = event.target.value;
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    this.delayTimeout = setTimeout(() => {
+      this.searchTerm = searchTerm;
+    }, 300);
+  }
+  get hasResults() {
+    return this.bears.data.length > 0;
+  }
 }
